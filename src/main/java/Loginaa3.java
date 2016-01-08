0dea957566809bab4b62f12be86c86793123d780
lcc <lcc@timevale.com>
2016-01-08 20:16:26 +0800

    aa

---
+++ src/main/java/Loginaa3.java
@@ -0,0 +1,53 @@
+/**
+ * 杭州天谷信息科技有限公司源代码，版权归杭州天谷信息科技有限公司所有 <br/>
+ * 项目名称：distance-converter <br/>
+ * 文件名：Loginaa.java <br/>
+ * 包：cn <br/>
+ */
+
+
+/**
+ * 类名：Loginaa.java <br/>
+ * 中文名：TODO <br/>
+ * 描述：TODO <br/>
+ * 修改历史： <br/>
+ * 1.[2016年1月8日下午8:08:10]创建文件 by lcc
+ */
+public class Loginaa3 {
+
+    public void maiaan(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("adf");
+    }
+
+    public void adfa5as(){
+        int a = 1;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        System.out.println("adfsf");
+    }
+
+
+    public void adf6aas(){
+        int a = 1;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        a=2;
+        System.out.println("adfsf");
+    }
+}
---
+++ src/test/java/TestLoginaa2.java
@@ -0,0 +1,12 @@
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLoginaa2 {
+    @Test
+    public void testAdfa3as()
+    {
+        Assert.assertEquals(8.04672,1);
+    }
+}
