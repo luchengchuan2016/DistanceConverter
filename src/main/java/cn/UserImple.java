6a9bdaa4924a865eb58a967a8d8c7f822a3a00b4
lcc <lcc@timevale.com>
2016-01-09 14:16:33 +0800

    a

---
+++ src/main/java/cn/UserImple.java
@@ -0,0 +1,22 @@
+/**
+ * 杭州天谷信息科技有限公司源代码，版权归杭州天谷信息科技有限公司所有 <br/>
+ * 项目名称：distance-converter <br/>
+ * 文件名：UserImple.java <br/>
+ * 包：cn <br/>
+ */
+package cn;
+
+/**
+ * 类名：UserImple.java <br/>
+ * 中文名：TODO <br/>
+ * 描述：TODO <br/>
+ * 修改历史： <br/>
+ * 1.[2016年1月9日下午2:16:10]创建文件 by lcc
+ */
+public class UserImple {
+
+    public void LoginUser(){
+        int a = 5;
+        System.out.println(5);
+    }
+}
