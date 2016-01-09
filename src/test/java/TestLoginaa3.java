0c96646acdedc072110a7d7dfa3a700e5af89c30
lcc <lcc@timevale.com>
2016-01-09 14:06:49 +0800

    dd

---
+++ src/test/java/TestLoginaa3.java
@@ -0,0 +1,17 @@
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLoginaa3 {
+    @Test
+    public void testAdfa3das()
+    {
+        DistanceConverter d = new DistanceConverter();
+        d.otherConverter();
+        Assert.assertEquals("a" ,"a");
+    }
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
